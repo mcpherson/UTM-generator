62d776b17c908e9084a0743fd98a4df1fdebda51
Marlon McPherson <mmcpherson5@cnm.edu>
2019-01-20 12:52:59 -0700

    Added URL field/validity check. Corrected spacing issues. Added 'clear' functionality

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 // buttons
-const engageSelect = document.getElementById('engage-select');
-const buildSelect = document.getElementById('build-select');
-const generate = document.getElementById('generate');
+const engageSelectBtn = document.getElementById('engage-select');
+const buildSelectBtn = document.getElementById('build-select');
+const generateBtn = document.getElementById('generate');
+const clearBtn = document.getElementById('generate');
 
 // button listeners
-engageSelect.addEventListener('click', engageDisplay);
-buildSelect.addEventListener('click', buildDisplay);
-generate.addEventListener('click', generateUTMs);
+engageSelectBtn.addEventListener('click', engageDisplay);
+buildSelectBtn.addEventListener('click', buildDisplay);
+generateBtn.addEventListener('click', generateUTMs);
+clearBtn.addEventListener('click', clearUTMs);
+
+// url input
+const urlInput = document.getElementById('url-input');
+
+// url input listener
+urlInput.addEventListener('blur', validURL);
 
 // engage stream title input
 const engageTitle = document.getElementById('engage-title');
@@ -34,6 +42,7 @@ function engageDisplay() {
   // display engage elements
   engageTitle.style.display = 'block';
   engageTitleLabel.style.display = 'inline';
+  // engageTitleLabel.style.marginBottom = '5px !important';
   
   // display appropriate number of name fields
   displayNumNameFields();
@@ -54,21 +63,37 @@ function buildDisplay() {
 
 
 
+// check validity of URL
+function validURL() {
+  if (urlInput.matches(':invalid')) {
+    alert('The URL you have entered is invalid.');
+  }
+}
+
+
+
 // display appropriate number of email title fields
 function displayNumNameFields() {
 
+  // thanks for reading my code!
   if(Number(numEmailsInput.value) === 42) {
-    document.body.innerHTML = "UNRESOLVED FINITE/INFINITE PARADOX ENCOUNTERED. YOU ARE NOT PREPARED TO TRANSCEND.";
-    function booted() {
-      setTimeout(function(){location.reload();}, 4200);
+    document.body.innerHTML = "<h1 style='text-align:center;'>UNRESOLVED FINITE / INFINITE PARADOX ENCOUNTERED.</h1> <h1 style='text-align:center;'>YOU ARE NOT PREPARED TO ASCEND.</h1> <h1 style='text-align:center;'>DIMENSIONAL LOCK REINITIALIZING ...</h1>";
+    function lockout() {
+      setTimeout(function() {location.reload();}, 5000);
     }
-    booted();
+    lockout();
     return;
   }
 
   // 'gotcha' for the cheeky ones
   if(numEmailsInput.value > 41) {
-    alert("You don't need that many emails. Go ahead and click 'OK' - no fancy DOM-based alert message for you.")
+    alert("You don't need that many emails. Go ahead and click 'OK' - no fancy DOM-based alert message for you. That'll teach you to question my UX design.")
+    numEmailsInput.value = 1;
+  }
+
+  // for the true trolls out there
+  if(Number(numEmailsInput.value) === 0 || Number(numEmailsInput.value) < 0) {
+    alert("Well, I guess your job is done, then.")
     numEmailsInput.value = 1;
   }
   
@@ -83,10 +108,11 @@ function displayNumNameFields() {
     newEmail = document.createElement('li');
     newEmail.innerHTML = `
     <label for="email-title-${i}" id="email-title-${i}-label">TITLE ${i}</label>
-    <input type="text" name="email-title-${i}" id="email-title-${i}">`;
+    <input type="text" name="email-title-${i}" id="email-title-${i}" data-lpignore="true">`;
 
     emailsList.appendChild(newEmail);
     
+    newEmail.setAttribute('autocomplete', 'new-password')
   }
 
   // Remove redundant '1' from title input lable when only one email is needed
@@ -107,5 +133,12 @@ function generateUTMs() {
 
 // display generated UTM codes in DOM
 function displayUTMs() {
+
+}
+
+
+
+// clear out generated UTMs
+function clearUTMs() {
 
 }
