e0fb28c2e3868eecf45254a9c42a9862d07b0093
Marlon McPherson <mmcpherson5@cnm.edu>
2019-01-19 23:22:45 -0700

    Created display functionality for the specified number of email title inputs.

--- app.js
+++ app.js
@@ -10,68 +10,124 @@ buildSelect.addEventListener('click', buildDisplay);
 const engageTitle = document.getElementById('engage-title');
 const engageTitleLabel = document.getElementById('engage-title-label');
 
-// number of engage emails inputs
-const numEngageEmailsInput = document.getElementById('num-engage-emails-input');
-const numEngageEmailsLabel = document.getElementById('num-engage-emails-label');
+// number of emails inputs
+const numEmailsInput = document.getElementById('num-emails-input');
+const numEmailsLabel = document.getElementById('num-emails-label');
 
-// listens for change in value of numEngageEmailsInput
-numEngageEmailsInput.addEventListener('change', displayNumEngageFields);
+// listens for change in value of numEmailsInput
+numEmailsInput.addEventListener('change', displayNumNameFields);
 
-// number of build emails inputs
-const numBuildEmailsInput = document.getElementById('num-build-emails-input');
-const numBuildEmailsLabel = document.getElementById('num-build-emails-label');
+// base title input
+const emailTitle = document.getElementById('email-title-1');
+const emailTitleLabel = document.getElementById('email-title-1-label');
 
-// listens for change in value of numBuildEmailsInput
-numBuildEmailsInput.addEventListener('change', displayNumBuildFields);
 
-// called when 'engage' is selected
+
+// called when 'engage' is clicked
 function engageDisplay() {
   
-  // add and remove classes from respective buttons
-  engageSelect.setAttribute('class', 'selected')
-  buildSelect.removeAttribute('class');
-  
   // display engage elements
-  numEngageEmailsInput.style.display = 'block';
-  numEngageEmailsLabel.style.display = 'inline';
   engageTitle.style.display = 'block';
   engageTitleLabel.style.display = 'inline';
-
-  // hide build elements
-  numBuildEmailsInput.style.display = 'none';
-  numBuildEmailsLabel.style.display = 'none';
   
   // display appropriate number of name fields
-  displayNumEngageFields();
+  displayNumNameFields();
 }
 
-// called when 'build' is selected 
+
+
+// called when 'build' is clicked 
 function buildDisplay() {
 
-  console.log('test');
-
-  // add and remove classes from respective buttons
-  buildSelect.setAttribute('class', 'selected')
-  engageSelect.removeAttribute('class');
-  
-  // display build elements
-  numBuildEmailsInput.style.display = 'block';
-  numBuildEmailsLabel.style.display = 'inline';
-
   // hide engage elements
-  numEngageEmailsInput.style.display = 'none';
-  numEngageEmailsLabel.style.display = 'none';
   engageTitle.style.display = 'none';
   engageTitleLabel.style.display = 'none';
   
   // display appropriate number of name fields
-  displayNumBuildFields();
+  displayNumNameFields();
 }
 
-function displayNumEngageFields() {
+
+
+// array of new email entries to be added
+var newEmailEntries = [];
+
+// display appropriate number of name fields
+function displayNumNameFieldss() {
+
+  // remove number from label for 1 email
+  if(numEmailsInput.value == 1) {
+    emailTitleLabel.innerHTML = "TITLE";
+  } else {
+    emailTitleLabel.innerHTML = "TITLE 1"
+  }
+
+  // 'gotcha' for the cheeky ones
+  if(numEmailsInput.value > 42) {
+    alert("You don't need that many emails.")
+  }
+
+  // list where extra email fields are inserted
+  var emailsList = document.getElementById("email-list");
+
+  // cuts array down to specified number of emails
+  newEmailEntries.length = ((numEmailsInput.value) - 1);
+
+  // display defined number of name fields
+  newEmailEntries.forEach(function(newEmailEntry) {
+  console.log('t');
+    // create new <li> to append to
+    let newEmail = document.createElement("li");
+
+    let i = 2
+
+    let newNameInput = `
+    <label for="email-title-${i}" id="email-title-${i}-label">TITLE ${i}</label>
+    <input type="text" name="email-title-${i}" id="email-title-${i}">`;
+
+    // appends defined number of entries to array
+    newEmailEntries.push(newNameInput);
+
+    // set content of new <li>
+    newEmail.innerHTML = newEmailEntries[i];
+
+    // append <li> to <ul>
+    emailsList.appendChild(newEmail);
+
+    i++;
+  });
+
+  
+}
+
+
+
+function displayNumNameFields() {
+
+  // 'gotcha' for the cheeky ones
+  if(numEmailsInput.value > 42) {
+    alert("You don't need that many emails.")
+  }
+
+  // list where extra email fields are inserted
+  var emailsList = document.getElementById("email-list");
+  
+  // clear name inputs to prevent duplication
+  while(emailsList.hasChildNodes()) {
+    emailsList.removeChild(emailsList.lastChild);
+  }
+
+  // adds new inputs depending on value of numEmailsInput
+  for (i=1; i<((Number(numEmailsInput.value)) + 1); i++) {
+
+    newEmail = document.createElement('li');
+    newEmail.innerHTML = `
+    <label for="email-title-${i}" id="email-title-${i}-label">TITLE ${i}</label>
+    <input type="text" name="email-title-${i}" id="email-title-${i}">`;
+
+    emailsList.appendChild(newEmail);
+    
+  }
 
 }
 
-function displayNumBuildFields() {
-
-}
